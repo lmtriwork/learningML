74bf83a8f966dcb7840022737e251daedd47a9cd
Trixy <lmtri.work@gmail.com>
2024-10-26 22:44:19 +0700

    adding function split train test

--- test.py
+++ test.py
@@ -27,3 +27,13 @@ housing.hist(bins = 50, figsize = (20,15))
 plt.show()
 
 import numpy as np
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set), "train +", len(test_set), "test")
